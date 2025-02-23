0798eae402603aa3ca4de778477337e004910b02
Sudheesh2409 <146453366+Sudheesh2409@users.noreply.github.com>
2025-02-23 13:28:57 +0530

    backedn commit

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 module.exports=function(req,res,next){
     try{
@@ -8,7 +9,7 @@ module.exports=function(req,res,next){
     if(!token){
         return res.status(400).send("Token not found")
     }
-    const decode = jwt.verify(token,"jwtkey")
+    const decode = jwt.verify(token,process.env.JWT_SECRET)
     req.user = decode.user
     next()
 }
