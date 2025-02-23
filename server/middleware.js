2102fbc3034f05db1474c501d909fd87695d6a00
Anisettysudheesh <anisettysudheesh@gmail.com>
2025-02-23 13:31:42 +0530

    Update middleware.js

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+require(dotenv).congif()
+
 
 module.exports=function(req,res,next){
     try{
@@ -8,7 +10,7 @@ module.exports=function(req,res,next){
     if(!token){
         return res.status(400).send("Token not found")
     }
-    const decode = jwt.verify(token,"jwtkey")
+    const decode = jwt.verify(token,process.env.JWT_SECRET)
     req.user = decode.user
     next()
 }
