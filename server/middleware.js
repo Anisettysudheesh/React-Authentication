beffe07b888d3204c3207eba8c65747f1b6077f6
Anisettysudheesh <anisettysudheesh@gmail.com>
2025-02-23 15:40:30 +0530

    Update middleware.js

--- server/middleware.js
+++ server/middleware.js
@@ -1,17 +1,22 @@
-const jwt = require('jsonwebtoken')
-module.exports=function(req,res,next){
-    try{
+const Register = require('./mongo'); // Import your User model
 
-    
-    let token = req.header("x-token")
-    if(!token){
-        return res.status(400).send("Token not found")
+module.exports = async function(req, res, next) {
+  try {
+    const userId = req.header("x-user-id"); // Get user ID from header
+    if (!userId) {
+      return res.status(400).send("User ID not found");
     }
-    const decode = jwt.verify(token,JWT_SECRET)
-    req.user = decode.user
-    next()
-}
-catch(err){
-    console.log(err);
-}
-}
+
+    // Verify that the user exists in the database
+    const user = await Register.findById(userId);
+    if (!user) {
+      return res.status(400).send("Invalid user");
+    }
+
+    req.user = { id: userId }; // Attach the user ID to the request
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("Invalid user");
+  }
+};
